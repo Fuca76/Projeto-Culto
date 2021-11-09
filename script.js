154447f0b1a5d2ef641553eec2d5fe5ceaf8c53f
Taís Carvalho <taisocarvalhocampos@gmail.com>
2021-11-09 00:11:58 -0300

    cor

--- script.js
+++ script.js
@@ -1,3 +1,2 @@
 alert('Graça e Paz!')
-var nome =window.prompt('Qual o seu nome ?')
-alert(`${nome} , que Deus continue abençoando a sua vida! \u{1F64F} `)
+alert(` Que Deus continue abençoando a sua vida! \u{1F64F} `) 
